3779057b9f8d94ab692ff0c359e52821dcd245cb
Awel7799 <awelkassim45@gmail.com>
2025-04-06 05:41:34 -0700

    menu for mobile and humberger just completed

--- demo-project/src/component/Header/Header.jsx
+++ demo-project/src/component/Header/Header.jsx
@@ -1,17 +1,23 @@
 // import statements remain unchanged
-import React from 'react';
+import React, { useState } from 'react';
 import cart from '../../assets/Group.svg';
 import menu from '../../assets/Vector (1).svg';
 import './Header.css';
 
 function Header() {
+  const [isOpen,setIsOpen]=useState(false)
+
+  const handleMenu = ()=>{
+    setIsOpen(!isOpen); 
+  }
   return (
     <nav className='navbar'>
       <div className='navbar-container'>
         <div>
           <p className='logo'>Your<span className='car-span'>Car</span></p>
         </div>
-
+       { 
+       isOpen &&(
         <div className='link-container'>
           <ul className='nav-links'>
             <li><a href="#">Home</a></li>
@@ -21,13 +27,16 @@ function Header() {
             <li><a href="#">Contact Us</a></li>
           </ul>
         </div>
+       )
+       }
         <div className='cart-icon'>
           <img src={cart} alt="shopping cart" />
           <p className='item-amount'>90</p>
         </div>
-
         <div className='menu-icon'>
-          <img src={menu} alt="menu" />
+          <button className='menu-btn' onClick={handleMenu} >
+            <img src={menu} alt="menu" />
+          </button>
         </div>
       </div>
     </nav>
