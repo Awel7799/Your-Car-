b86a1e5622371b1cd4884e28dad590655d26b179
Awel7799 <awelkassim45@gmail.com>
2025-04-07 05:30:24 -0700

    just completed basic interactivity and responsiveness

--- demo-project/src/component/cartContext/CartProvider.jsx
+++ demo-project/src/component/cartContext/CartProvider.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import cartContext from "./cartContext";
 
 const CartProvider = ({ children }) => {
-    const [carts, setCart] = useState([{ id: 1, name: 'Toyota Corolla', price: 12000 },
-        { id: 2, name: 'Honda Civic', price: 14000 },]);
+    const [carts, setCart] = useState([{ id: 1, name: 'Toyota Corolla', price: 12000 ,discription:'car discription'},
+        { id: 2, name: 'Honda Civic', price: 14000 },
+        { id: 2, name: 'Honda Civic', price: 14000 }]);
         const [isCartOpen, setIsCartOpen]=useState(false);
         function handlecart(){
            setIsCartOpen(!isCartOpen);
--- demo-project/src/component/cartDetail/cartDetial.jsx
+++ demo-project/src/component/cartDetail/cartDetial.jsx
@@ -1,22 +1,44 @@
 import './cartDetail.css';
 import cartContext from '../cartContext/cartContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 
 function cartDetail({ isCartOpen }){
 
-    const {cart: carts,clearCart}=useContext(cartContext);
+    const {cart: carts,clearCart,removeFromCart}=useContext(cartContext);
 
 if(!isCartOpen) return null;
+   
+   const [count,setCount]=useState(0);
+   const increase=()=>{
+    setCount(count+1);
+   }
+
+   const decrease=()=>{
+    setCount(count-1);
+   }
 
     return(
 <div className='cartDetailContrainer'>
    <div className='item-section'>
      {carts.length===0  ? (<p>your cart is empty</p>):
-     (carts.map((item)=>(<div key={item.id} className='cart-item'>
-        <p>{item.name}</p>
-        <p>price : {item.price}</p>
-     </div>)))}
+     (carts.map((item)=>(
+       <div className='individual-item'>
+            <div className='item-discription'>
+              <p className='item-name'>{item.name}</p>
+              <p className='item-text'>{item.discriptio}</p>
+              <div className='inc-dec-btn'>
+                    <button className='dec-btn' onClick={decrease}>-</button>
+                    <p>{count}</p>
+                    <button className='inc-btn' onClick={increase}>+</button>
+                    <button  onClick={()=>removeFromCart(item.id)}>🗑️</button>
+              </div>
+            </div>
+            <div className='item-img'>
+
+            </div>
+       </div>
+        )))}
    </div>
    <div>
         <button className='delete-btn'
