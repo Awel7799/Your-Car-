b06fe5650fedd7843bd9d4c80ff0b60a2bda7ba4
Awel7799 <awelkassim45@gmail.com>
2025-04-13 05:18:41 -0700

    the carousel part of the website just completed

--- demo-project/src/component/cars/carousel.jsx
+++ demo-project/src/component/cars/carousel.jsx
@@ -1,11 +1,14 @@
 import './carousel.css'
-import React ,{useState} from 'react';
+import carImage from '../../assets/Group 1133.png'
+import React ,{useState , useEffect} from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { useProductContext } from '../cartContext/productContext';
 function carousel(){
 
     const {products}=useProductContext();
     const [startIndex,setStartIndex]=useState(0);
-    const visibleCount=1;
+    const isMobile = useMediaQuery({ maxWidth: 700 });
+    const visibleCount = isMobile ? 1 : 3;
 
     const handleNext =()=>{
         if(startIndex + visibleCount<products.length){
@@ -21,6 +24,14 @@ function carousel(){
 
     const visibleProducts=products.slice(startIndex,startIndex+visibleCount)
 
+    const [count,setCount]=useState(0);
+    const increase=()=>{
+     setCount(count+1);
+    }
+
+const decrease=()=>{
+    setCount(count-1);
+   }
 
     return(
      <div className='general-car-container'>
@@ -32,6 +43,7 @@ function carousel(){
                <p className='p-small-car-tittle'>Cars</p>
             </div>
          </div>
+         <div className='right-left-btn'>
          <button className='prv-btn' onClick={handlePrev}> left</button>
          { visibleProducts.map((product)=>(
              <div key={product.id} className='card-list'>
@@ -39,14 +51,24 @@ function carousel(){
                  <img className='car-img' src={product.image} alt={product.name} />
              </div>
              <div className='cards-discription'>
-                 {product.description}
+                <p className='product-name'>{product.name}</p>
+                <p className='product-brand'>{product.brand}</p>
+                 <p className='p-cards-discription'>{product.description}</p>
              </div>
              <div className='cards-button'>
-  
+                <div className='seat-No'>
+                      <img src={carImage} alt="" />
+                </div>
+                <div className='No-of-item'>
+                    <button className='dec-btn' onClick={decrease}>-</button>
+                    <p className='counter'>{count}</p>
+                    <button className='inc-btn' onClick={increase}>+</button>
+                </div>
              </div>
            </div>
          ))}
           <button className='Next-btn' onClick={handleNext}> right</button>
+         </div>
      </div>
     );
 };
