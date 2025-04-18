f9ec8abe31ce8740dfe63404441bf82f0c36e56a
Awel7799 <awelkassim45@gmail.com>
2025-04-18 00:52:28 -0700

    car gallary detailed correction completed

--- demo-project/src/component/carGallary/Gallary.jsx
+++ demo-project/src/component/carGallary/Gallary.jsx
@@ -3,7 +3,6 @@ import img1 from '../../assets/Car.png';
 import img2 from '../../assets/Car (4).png';
 import img3 from '../../assets/Car (1).png';
 import img4 from '../../assets/Car (2).png';
-import img5 from '../../assets/Rectangle 2.png';
 import img6 from '../../assets/Car (3).png';
 import img7 from '../../assets/Car (5).png';
 import img8 from '../../assets/Car (4).png';
@@ -15,16 +14,20 @@ return(
      <div className='Gallary-container'>
        <div className='first-Gallary'>
            <img src={img1} alt="first car" />
-           <img src={img2} alt="second car" />
-           <img src={img3} alt="third car" />
+           <img src={img4} alt="second car" />
+           <img src={img7} alt="third car" />
        </div>
        <div className='second-Gallary'>
-            <img src={img4} alt="fifth car" />
-            <img src={img5} alt="fifth car" />
+            <img src={img2} alt="fifth car" />
+            <div className='tesla-model'>
+               <p className='tesla-tittle'>Tesla Model 3</p>
+               <p className='tesla-discription'>Disruptive, avant-garde, futuristic, innovative.</p>
+               <button className='tesla-contact-btn'>Contact</button>
+            </div>
             <img src={img6} alt="sixth car" />
        </div>
        <div className='third-Gallary'>
-             <img src={img7} alt="seventh car" />
+             <img src={img3} alt="seventh car" />
              <img src={img8} alt="eighth car" />
              <img src={img9} alt="tenth car" />
        </div>
